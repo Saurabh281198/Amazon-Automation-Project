f4a6da3326c3bb7a378c9a988ffd1cb32e878943
Saurabh Verma <161503654+Saurabh281198@users.noreply.github.com>
2024-03-05 20:26:08 +0530

    Add files via upload

--- src/main/java/com/PageClasses/AddressPage.java
+++ src/main/java/com/PageClasses/AddressPage.java
@@ -25,6 +25,51 @@ public class AddressPage extends AbstractClass {
     @FindBy(css = ".a-popover-trigger")
     WebElement deliverToUser;
 
+    @FindBy(linkText = "Add an address or pick-up point")
+    WebElement addAnAddress;
+
+    @FindBy(css = ".a-section.a-spacing-none.address-plus-icon.aok-inline-block")
+    WebElement addAddressPlusIcon;
+
+    @FindBy(css = "#address-ui-widgets-countryCode span[data-action='a-dropdown-button']")
+    WebElement countryNameElement;
+
+    @FindBy(xpath = "//a[contains(@id,'address-ui-widgets-countryCode')]")
+    List<WebElement> countryNamesList;
+
+    @FindBy(id = "address-ui-widgets-enterAddressFullName")
+    WebElement fullNameElement;
+
+    @FindBy(id = "address-ui-widgets-enterAddressPhoneNumber")
+    WebElement mobileNoElement;
+
+    @FindBy(id = "address-ui-widgets-enterAddressPostalCode")
+    WebElement pincodeElement;
+
+    @FindBy(id = "address-ui-widgets-enterAddressLine1")
+    WebElement buildingNameElement;
+
+    @FindBy(id = "address-ui-widgets-enterAddressLine2")
+    WebElement areaNameElement;
+
+    @FindBy(id = "address-ui-widgets-landmark")
+    WebElement landmarkElement;
+
+    @FindBy(id = "address-ui-widgets-enterAddressCity")
+    WebElement cityNameElement;
+
+    @FindBy(xpath = "//a[contains(@id,'address-ui-widgets-enterAddressStateOrRegion')]")
+    List<WebElement> stateNamesList;
+
+    @FindBy(css = "#address-ui-widgets-enterAddressStateOrRegion .a-button-text.a-declarative")
+    WebElement stateNameElement;
+
+    @FindBy(css = "#address-ui-widgets-form-submit-button .a-button-input")
+    WebElement submitElement;
+
+    @FindBy(css = "#yaab-alert-box .a-alert-heading")
+    List<WebElement> addressSuccessElement;
+
     public void clickOnAppropriateAddress(String addressKeyword) throws InterruptedException {
         for (int i = 0; i < addressDetailList.size(); i++) {
             if (addressDetailList.get(i).getAttribute("aria-label").contains(addressKeyword)) {
@@ -39,6 +84,46 @@ public class AddressPage extends AbstractClass {
         boolean value = addressDetailList.size() == 0;
         if (value == false) {
             System.out.println("Select appropriate keyword. Currently the address in not present!!");
+        }
+    }
+
+    public void updateNewAddress(String countryName, String fullName, String mobileNo, String pincode,
+            String BuildingAddress, String area, String landmark, String city, String stateName) {
+        waitForWebElementToAppear(addAnAddress);
+        addAnAddress.click();
+        addAddressPlusIcon.click();
+        countryNameElement.click();
+        for (int i = 0; i < countryNamesList.size(); i++) {
+            if (countryNamesList.get(i).getText().trim().equalsIgnoreCase(countryName)) {
+                countryNamesList.get(i).click();
+                break;
+            }
+        }
+        fullNameElement.sendKeys(fullName);
+        mobileNoElement.sendKeys(mobileNo);
+        pincodeElement.clear();
+        pincodeElement.sendKeys(pincode);
+        buildingNameElement.sendKeys(BuildingAddress);
+        areaNameElement.sendKeys(area);
+        landmarkElement.sendKeys(landmark);
+        cityNameElement.clear();
+        cityNameElement.sendKeys(city);
+        stateNameElement.click();
+        for (int i = 0; i < stateNamesList.size(); i++) {
+            if (stateNamesList.get(i).getText().trim().equalsIgnoreCase(stateName)) {
+                stateNamesList.get(i).click();
+                break;
+            }
+        }
+        submitElement.click();
+    }
+
+    public void verifyIfAddressIsUpdated() {
+        boolean value = addressSuccessElement.size() == 0;
+        if (value == false) {
+            System.out.println(addressSuccessElement.get(0).getText());
+        } else {
+            System.out.println("The address is not correctly updated!!");
         }
     }
 }
--- src/test/java/com/amazon/TestClasses/AddressVerficationAndUpdation.java
+++ src/test/java/com/amazon/TestClasses/AddressVerficationAndUpdation.java
@@ -1,6 +1,8 @@
 package com.amazon.TestClasses;
 
 import java.io.IOException;
+import java.util.HashMap;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import com.PageClasses.AddressPage;
 import com.PageClasses.HomePage;
@@ -8,23 +10,52 @@ import com.PageClasses.LoginPage;
 import com.amazon.BaseTestClass.BaseTest;
 import com.amazon.BaseTestClass.RetryClass;
 
-public class AddressVerficationAndUpdation extends BaseTest{
+public class AddressVerficationAndUpdation extends BaseTest {
 
-    @Test(retryAnalyzer = RetryClass.class)
-    public void addressTest() throws InterruptedException, IOException {
-        
-        //Basic Details
-        String mobileNo = "+919993433823";
-        String password = "Saurabhv@28";
-        
-        //Steps
+    @Test(retryAnalyzer = RetryClass.class,dataProvider = "getData")
+    public void verifyAddressAndSelect(HashMap<String, String> data) throws InterruptedException, IOException {
+
+        // Steps
         HomePage homePage = goToHomePageClass();
         LoginPage loginPage = homePage.goToLoginPage();
-        homePage = loginPage.sendDataAndVerifyLoginStatus(mobileNo, password);
+        homePage = loginPage.sendDataAndVerifyLoginStatus(data.get("loginMobileNo"),data.get("password"));
         AddressPage addressPage = homePage.verifyAddressAndChange();
-        addressPage.clickOnAppropriateAddress("Sankalp home");
+        addressPage.clickOnAppropriateAddress("Classic Garden");
         takeScreenshot("amazonHomePage", driver);
         addressPage.verifyIfAddressIsSelected();
+    }
+
+    @Test(dataProvider = "getData",retryAnalyzer = RetryClass.class)
+    public void updateNewAddress(HashMap<String, String> data) throws InterruptedException {
+
+        // Steps
+        HomePage homePage = goToHomePageClass();
+        LoginPage loginPage = homePage.goToLoginPage();
+        homePage = loginPage.sendDataAndVerifyLoginStatus(data.get("loginMobileNo"),data.get("password"));
+        AddressPage addressPage = homePage.verifyAddressAndChange();
+        addressPage.updateNewAddress(data.get("Country"), data.get("FullName"), data.get("MobileNo"),
+                data.get("PinCode"), data.get("BuildingAddress"), data.get("Area"), data.get("Landmark"),
+                data.get("City"), data.get("State"));
+        addressPage.verifyIfAddressIsUpdated();
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("loginMobileNo", "+919993433823");
+        map.put("password", "Saurabhv@28");
+        map.put("Country", "India");
+        map.put("FullName", "Harini G");
+        map.put("MobileNo", "9876543210");
+        map.put("PinCode", "411046");
+        map.put("BuildingAddress", "C-4 Classic Garden");
+        map.put("Area", "Katraj");
+        map.put("Landmark", "Opp. BVP University");
+        map.put("City", "Pune");
+        map.put("State", "MAHARASHTRA");
+
+        return new Object[][] { { map } };
     }
 
 }
